b6f3ef8805554a6a0d4a55b582473744590e2886
Ramiro Nilson Barros <ramiro.nilson.barros@gmail.com>
2023-07-13 17:45:36 -0300

    Add status code factory

---
+++ http_error_schemas/factory.py
@@ -0,0 +1,15 @@
+import schemas
+
+HTTP_ERRORS = {
+    422: schemas.RequestValidationError,
+    409: schemas.ConflictError,
+    401: schemas.UnauthorizedError,
+    404: schemas.NotFoundError,
+}
+
+
+def get_error_class(status_code: int):
+    if status_code in HTTP_ERRORS:
+        return HTTP_ERRORS.get(status_code)
+    else:
+        raise ValueError(f"Status code no in schemas: {status_code}")
